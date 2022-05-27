d31296e667bbe37205ada8ed53f8f7a3f15a74aa
Ayatoo047 <ajibadeyakubadebare@gmail.com>
2022-05-27 22:28:08 +0100

    updates

--- blogpost/settings.py
+++ blogpost/settings.py
@@ -9,10 +9,12 @@ https://docs.djangoproject.com/en/4.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
-
+import os
 from pathlib import Path
-# from decouple import config
 
+# from base.views import blogpost
+from decouple import config
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -21,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '-w85d=domr4y)qh7cxehyjzo!_rqi)oli4*s!k+fi+i+y75=_u'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
